4b5671ac2ef568efacb11edea50367df73052976
Chris Menezes <chris@menezes.ca>
2019-05-01 21:55:13 -0400

    refactor dictionary

---
+++ dictionary/lib/dictionary/word_list.ex
@@ -0,0 +1,25 @@
+defmodule Dictionary.WordList do
+  
+  @word_list_file_name "assets/words.txt"
+
+  def random_word(word_list) do
+    word_list
+    |> Enum.random
+    |> String.trim
+  end
+
+  def words_of_length(word_list, len) do
+    word_list
+    |> Stream.map(&String.trim/1)
+    |> Enum.filter(&(String.length(&1) == len))
+  end
+
+  def start() do
+    @word_list_file_name
+    |> Path.expand(__DIR__ <> "/../..")
+    |> File.open! 
+    |> IO.read(:all) 
+    |> String.split("\n")
+  end
+
+end
