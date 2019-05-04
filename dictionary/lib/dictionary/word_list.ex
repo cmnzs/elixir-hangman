be3bb9eb241e9cb7c90d7a6de17cda5b6f7479b8
Chris Menezes <chris@menezes.ca>
2019-05-03 23:28:30 -0400

    make word list use an agent

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -2,10 +2,19 @@ defmodule Dictionary.WordList do
   
   @word_list_file_name "assets/words.txt"
 
-  def random_word(word_list) do
-    word_list
-    |> Enum.random
-    |> String.trim
+  def start_link() do
+    Agent.start_link(&word_list/0)
+  end
+
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
+  end
+
+  def word_list() do
+    @word_list_file_name
+    |> Path.expand(__DIR__ <> "/../..")
+    |> File.read!()
+    |> String.split("\n")
   end
 
   def words_of_length(word_list, len) do
@@ -13,12 +22,4 @@ defmodule Dictionary.WordList do
     |> Stream.map(&String.trim/1)
     |> Enum.filter(&(String.length(&1) == len))
   end
-
-  def start() do
-    @word_list_file_name
-    |> Path.expand(__DIR__ <> "/../..")
-    |> File.read!()
-    |> String.split("\n")
-  end
-
 end
