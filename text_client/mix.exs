25c3bdb02050fd202a18ae2f708902bdfa1d90c3
Chris Menezes <cmenezes@pagerduty.com>
2019-06-03 10:24:41 -0400

    don't start the hangman server process in the text_client. "included_applications" does this. Note also: for the most part we don't have to worry about this, as frameworks handle this for us

--- text_client/mix.exs
+++ text_client/mix.exs
@@ -5,7 +5,7 @@ defmodule TextClient.MixProject do
     [
       app: :text_client,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,7 +14,8 @@ defmodule TextClient.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      included_applications: [:hangman],
+      extra_applications: [:logger],
     ]
   end
 
